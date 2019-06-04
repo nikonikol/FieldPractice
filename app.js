b9e885b6570df968de26a7b2d8c6ac5dfee1d196
qq1257787262 <1257787262@qq.com>
2019-06-04 11:08:54 +0800

    第一次

---
+++ app.js
@@ -0,0 +1,32 @@
+var express=require('express')
+var fs=require('fs')
+var session=require('express-session')
+var router=require('./router')
+var bodyParser=require('body-parser')
+
+var app=express()
+//配置模板引擎
+app.engine('html',require('express-art-template'))
+//公开文件夹
+app.use('/node_modules/',express.static('./node_modules'))
+app.use('/public/',express.static('./public'))
+//配置body=parser
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use(session({
+    // 配置加密字符串，它会在原有加密基础之上和这个字符串拼起来去加密
+    // 目的是为了增加安全性，防止客户端恶意伪造
+    secret: 'itcast',
+    resave: false,
+    saveUninitialized: false // 无论你是否使用 Session ，我都默认直接给你分配一把钥匙
+  }))
+//把路由容器挂载到app服务中
+app.use(router)
+//设置404页面
+app.use(function(req,res,next){
+    res.render('404.html')
+})
+//监听窗口
+app.listen(4000,function(){
+    console.log('fighting`````')
+})
---
+++ router.js
@@ -0,0 +1,113 @@
+/**
+ * router.js路由模块
+ * 职责
+ *  处理登陆功能路由
+ *  根据不同请求响应url
+ */
+
+ //加载文件
+ var fs = require('fs')
+ //加载express，包装路由
+ var express=require('express')
+ //加载mysql.js
+ var mysql=require('./models/mysql')
+ //加载md5插件
+ var md5=require('blueimp-md5')
+ //创建一个路由容器
+ var router=express.Router()
+
+
+ /*
+ *
+ //将登陆路由挂载到router容器里
+ *
+ */
+
+
+/* 登陆路由*/
+//渲染登陆页面
+ router.get('/',function(req,res){
+     res.render('login.html')
+ })
+
+//登录请求
+ router.post('/',function(req,res){
+     //获取请求体
+     var userid=req.body.UserId
+     var password=req.body.Password
+     console.log(req.body)
+     var sql=null
+    //匹配登陆账号和密码
+    try{
+        sql=`SELECT
+        studentinfo.UserId,
+        studentinfo.Password
+    FROM
+        studentinfo
+    WHERE
+        studentinfo.UserId="`+userid+`" AND
+        studentinfo.Password="`+password+`"`
+        mysql(sql,function(err,data){
+            if(err){
+                console.log('11')
+                return res.status(500).json({
+                    err_code: 500,
+                    message: err.message
+                })
+            }
+            // 如果账号和密码匹配，则 Userinformation 是查询到的用户对象，否则就是 null
+            if(!data){
+                return res.status(200).json({
+                    //提供错误码
+                    err_code: 1,
+                    message: 'nickname or password is invalid.'
+                })
+            }
+            else{
+                req.session.Userinformation = data
+                console.log(req.session.Userinformation)
+                // 用户存在，登陆成功，通过 Session 记录登陆状态
+                res.status(200).json({
+                    err_code: 0,
+                    message: 'OK'
+                })
+            }
+            
+        })
+    }
+    catch(err){
+        console.log('err')
+        res.status(500).json({
+            code:2,
+            err: err.message,
+            message: ''
+        })
+    }
+ })
+
+ /* index路由*/
+
+router.get('/index',function(req,res){
+    //渲染页面
+    if (req.session.Userinformation === null || req.session.Userinformation === undefined) {
+        return res.redirect('/login')
+    }
+    res.render('index.html', {
+        Userinformation: req.session.Userinformation
+    })
+})
+
+
+
+/* 注册路由*/
+ router.get('/register', function (req, res) {
+    res.render('register.html')
+})
+
+//注册请求
+router.post('/register', async function (req, res) {
+    var register=req.body
+    
+})
+
+module.exports=router
